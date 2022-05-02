af376d32c06d6f1684587ab3745698b23fc3ab7d
EmiriMaegawa <101238309+mgwem@users.noreply.github.com>
2022-05-02 16:28:27 +0900

    [Update]children演習

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.box1').on('click',function(){
-    $(this).slideUp();
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
   });
 });
